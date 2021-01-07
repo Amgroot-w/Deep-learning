2fc5677f2f83426e97a9af012a397137063c4609
Zhang Jingchuan <57850924+Amgroot-w@users.noreply.github.com>
2021-01-07 16:22:42 +0800

    ex4 大作业 FINISH

--- ex4_compare_SVM.py
+++ ex4_compare_SVM.py
@@ -2,31 +2,42 @@
 import numpy as np
 import pandas as pd
 from sklearn import svm
-from tensorflow.examples.tutorials.mnist import input_data
-import time
+from scipy.io import loadmat
 
-start = time.time()
 # 导入数据
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-np.random.shuffle(mnist.train)  # 洗牌
-np.random.shuffle(mnist.test)  # 洗牌
+data_x = loadmat('MNIST.mat')['X']
+data_y = loadmat('MNIST.mat')['y']
+data = np.column_stack((data_x, data_y))
 
-# 训练
-clf = svm.SVC(kernel='rbf', C=0.3, gamma=70)
-train_num = 5000
-print('\nSVM拟合中...')
-clf.fit(mnist.train.images[:train_num, :], mnist.train.labels[:train_num])
-print('拟合完成！')
+test_ac = []
+for i in range(10):
+    np.random.shuffle(data)  # 洗牌
+    m = data.shape[0]  # 样本总数
+    n = data.shape[1]  # 特征数
+    images = data[:, :n-1]  # 图片
+    labels = data[:, n-1]  # 标签
 
-# 测试
-test_num = 5000
-pred = clf.predict(mnist.test.images[:test_num, :])
-ac = np.mean(np.equal(pred, mnist.test.labels))
+    # 分配训练集、测试集
+    train_num = int(m * 0.7)  # 训练样本数
+    test_num = int(m * 0.3)   # 测试样本数
+    train_x = images[:train_num, :]
+    train_y = labels[:train_num]
+    test_x = images[train_num:m, :]
+    test_y = labels[train_num:m]
 
-end = time.time()
-print('用时：%.2f秒' % (end-start))
+    # 训练
+    clf = svm.SVC(C=0.12)
+    clf.fit(train_x, train_y)
 
+    # 测试
+    ac = clf.score(train_x, train_y)
+    ac1 = clf.score(test_x, test_y)
 
+    # 记录
+    test_ac.append(ac)
+    print('第%d次实验，测试集准确率：%.4f' % (i, ac))
+
+pd.DataFrame(test_ac).to_csv(r'Tables\svm.csv', index=False)
 
 
 
